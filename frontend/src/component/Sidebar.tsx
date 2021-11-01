ef9a0124f69f47e8b5f9826ae967abcfed8fc44c
gidskql6671 <gidskql6671@gmail.com>
2021-11-01 23:52:19 +0900

    [Feat] 사이드바를 구현함
    TODO 네비게이션 기능 추가
    TODO 서버로부터 디렉토리 정보를 받아와 네비게이션에 추가

---
+++ frontend/src/component/Sidebar.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as ExpandOnSvg } from '../asset/image/expand_more.svg';
+import { ReactComponent as ExpandOffSvg } from '../asset/image/chevron_left.svg';
+import { ReactComponent as StarSvg } from '../asset/image/star.svg';
+
+const Container = styled.div`
+	width: 300px;
+	height: 100%;
+	background-color: ${props => props.theme.color.SecondaryBG};
+	border-right: 1px solid ${props => props.theme.color.Line};
+	
+	padding: 25px;
+`;
+
+const CapacityContainer = styled.div`
+	margin-bottom: 50px;
+`;
+
+const ProgressBar = styled.div`
+	background-color: #C4C4C4;
+	height: 6px;
+	
+	border-radius: 10px;
+	margin-bottom: 8px;
+`;
+const Progress = styled.div<{percent: number}>`
+	background-color: ${props => props.theme.color.Primary};
+	height: 6px;
+	width: ${props => `${props.percent}%`};
+	
+	border-radius: 10px;
+`;
+const ProgressValue = styled.p`
+	margin: 0;
+	font-weight: bold;
+`;
+
+const NavBar = styled.nav`
+	
+`;
+const NavBox = styled.div`
+	margin: 10px 0;
+	
+	display: flex;
+	align-items: center;
+`;
+const Nav = styled.p`
+	font-size: 24px;
+	margin: 0;
+	margin-right: 10px;
+	
+	cursor: pointer;
+`;
+
+interface Props {
+	currentCapacity: number;
+	maxCapacity: number;
+}
+
+const Sidebar: React.FC<Props> = ({ currentCapacity, maxCapacity }) => {
+	return (
+		<Container>
+			<CapacityContainer>
+				<ProgressBar>
+					<Progress percent={currentCapacity / maxCapacity * 100} />
+				</ProgressBar>
+				<ProgressValue>
+					{`${currentCapacity}Byte / ${maxCapacity}Byte`}
+				</ProgressValue>
+			</CapacityContainer>
+			<NavBar>
+				{/* 추후 Link로 변경하면 될듯 */}
+				<NavBox> 
+					<Nav> 내 디렉토리 </Nav>
+					<ExpandOnSvg />
+				</NavBox>
+				<NavBox> 
+					<Nav> 중요 문서함 </Nav>
+					<StarSvg />
+				</NavBox>
+				<NavBox> 
+					<Nav> 휴지통 </Nav>
+				</NavBox>
+			</NavBar>
+		</Container>
+	);
+}
+
+export default React.memo(Sidebar);
