6d2fe61364a47c9c6703844d7bc4ffd0e8e6e0cf
gidskql6671 <gidskql6671@gmail.com>
2021-11-01 16:28:09 +0900

    [Feat] Global Style & Theme 설정

---
+++ frontend/src/asset/style/styled.d.ts
@@ -0,0 +1,13 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		color: {
+			PrimaryBG: string;
+			SecondaryBG: string;
+			Primary: string;
+			Line: string;
+		},
+		HeaderHeight: string;
+	}
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './asset/style/theme';
+import GlobalStyle from './asset/style/global-styles';
 import App from './App';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={defaultTheme}>
+			<GlobalStyle />
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
