faaabd285378a68cd3c3f31249c7e90a4e89354e
gidskql6671 <gidskql6671@gmail.com>
2021-11-01 23:51:37 +0900

    [Feat] 메인페이지의 상단 메뉴바를 제작함
    전체 선택, 업로드, 공유하기 버튼이 위치하는 메뉴바 제작

---
+++ frontend/src/component/FileMenu.tsx
@@ -0,0 +1,62 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as ToggleOffSvg } from '../asset/image/check_box_outline_blank.svg';
+import { ReactComponent as ToggleOnSvg } from '../asset/image/check_box_outline_selected.svg';
+
+
+const Container = styled.div`
+	padding: 15px 20px;
+	border-bottom: 1px solid ${props => props.theme.color.Line};
+	
+	display: flex;
+`;
+
+const SelectAllBtn = styled.button`
+	cursor: pointer;
+	
+	padding: 0;
+	margin-right: 20px;
+	
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+
+const ActButton = styled.button`
+	cursor: pointer;
+	outline: none;
+	border: 1px solid ${props => props.theme.color.Line};
+	border-radius: 5px;
+	background-color: ${props => props.theme.color.SecondaryBG};
+	width: 150px;
+`; 
+
+const ShareButton = styled.button`
+	cursor: pointer;
+	outline: none;
+	border: 1px solid ${props => props.theme.color.Line};
+	border-radius: 5px;
+	background-color: ${props => props.theme.color.SecondaryBG};
+	width: 150px;
+	
+	margin-left: auto;
+`;
+
+interface Props {
+	showShareButton? : boolean;
+}
+
+const FileMenu: React.FC<Props> = ({ showShareButton }) => {
+	return (
+		<Container>
+			<SelectAllBtn>
+				<ToggleOffSvg />
+			</SelectAllBtn>
+			<ActButton> 올리기 </ActButton>
+			{ !showShareButton || <ShareButton> 공유하기 </ShareButton> }
+		</Container>
+	);
+}
+
+export default React.memo(FileMenu);
