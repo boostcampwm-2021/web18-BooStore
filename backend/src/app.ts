48f4bb0bf95417aa6fc499439908f0fc16b51bc7
gidskql6671 <gidskql6671@gmail.com>
2021-11-03 14:31:46 +0900

    [Feat] 구현된 로그인 및 회원가입 기능을 서버에 등록함

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,11 @@ import * as path from 'path';
 import * as cookieParser from 'cookie-parser';
 import * as logger from 'morgan';
 import * as dotenv from 'dotenv';
-import indexRouter from './routes/index';
-import { Cloud } from './model';
+import * as passport from 'passport';
+import passportConfig from './config/passport';
+import * as session from 'express-session';
+
+import { authRouter } from './routes/index';
 
 const app = express();
 dotenv.config();
@@ -13,6 +16,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(path.resolve(), '../frontend/build')));
-app.use('/', indexRouter);
+
+app.use(session({ secret: 'secret123123', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+passportConfig();
+
+app.use('/', authRouter);
+app.use('*', (req, res) => {
+	res.sendFile('index.html', {
+		root: path.join(__dirname, '../../frontend/build')
+	});
+});
 
 module.exports = app;
