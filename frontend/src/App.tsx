2b08adf9702bbe247828061838a57e1f9fa15d35
gidskql6671 <gidskql6671@gmail.com>
2021-11-08 19:15:21 +0900

    [Fix] 새로고침시, 로그인이 유지되지않던 버그를 수정함
    새로고침할시, 서버로부터 로그인 여부를 받아와 처리하도록 구현함
    useEffect에서 이를 수행하기에 한번 렌더링된 이후 로그인 여부가 체크된다.
    Suspense나 LocalStorage로 추후에 개선해보자

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './Router';
 import Header from './component/Header';
@@ -6,10 +6,31 @@ import { User } from './model';
 
 const App: React.FC = () => {
 	const [user, setUser] = useState<User | null>(null);
+
+	useEffect(() => {
+		fetch(`/user`, {
+			credentials: 'include'
+		})
+		.then((response) => {
+			if (response.ok) {
+				return response.json();
+			} else if (response.status === 401) {
+				return null;
+			} else {
+				throw new Error(response.status.toString());
+			}
+		})
+		.then((data) => {
+			setUser(data);
+		})
+		.catch((err) => console.log(err));
+	}, []);
+
 	return (
 		<>
 			<BrowserRouter>
 				<Header user={user} />
+
 				<Router user={user} setUser={setUser} />
 			</BrowserRouter>
 		</>
