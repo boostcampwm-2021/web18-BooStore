67ae11294b98f0390fd800d8f94c7adb57ba19f3
K-moovie <kyha97@naver.com>
2021-11-16 19:04:16 +0900

    [Style] 파일 아이콘 맵핑 기능 수정
    파일 아이콘을 변경해서 더 다양한 타입의 파일을 표현할 수 있습니다.

--- frontend/src/component/fileManagement/TypeIcon.tsx
+++ frontend/src/component/fileManagement/TypeIcon.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ReactComponent as Folder } from '../../asset/image/icon_folder.svg';
-import { ReactComponent as File } from '../../asset/image/icon_file.svg';
-import { ReactComponent as Image } from '../../asset/image/icon_image.svg';
-import { ReactComponent as Pdf } from '../../asset/image/icon_pdf.svg';
-import { ReactComponent as Zip } from '../../asset/image/icon_zip.svg';
-import { ReactComponent as Text } from '../../asset/image/icon_text.svg';
+import { ReactComponent as Folder } from '../../asset/image/icons/icon_folder.svg';
+import { ReactComponent as TextFile } from '../../asset/image/icon_text.svg';
+import { ReactComponent as DefaultFile } from '../../asset/image/icons/icon_file.svg';
+import { ReactComponent as DocsFile } from '../../asset/image/icons/icon_docs.svg';
+import { ReactComponent as ImageFile } from '../../asset/image/icons/icon_image.svg';
+import { ReactComponent as VideoFile } from '../../asset/image/icons/icon_video.svg';
+import { ReactComponent as AudioFile } from '../../asset/image/icons/icon_audio.svg';
+import { ReactComponent as ZipFile } from '../../asset/image/icons/icon_zip.svg';
+
 interface Props {
 	type: string;
 }
 const TypeIcon: React.FC<Props> = ({ type }) => {
 	return (
 		<IconDiv>
-			{type.includes('folder') && <Folder style={{ width: '40px', height: '40px' }} />}
-			{type.includes('text') && <Text style={{ width: '40px', height: '40px' }} />}
-			{type.includes('image') && <Image style={{ width: '40px', height: '40px' }} />}
-			{type.includes('pdf') && <Pdf style={{ width: '40px', height: '40px' }} />}
-			{type.includes('zip') && <Zip style={{ width: '40px', height: '40px' }} />}
+			{type.includes('folder') && <Folder />}
+			{type.includes('text') && <TextFile />}
+			{type.includes('image') && <ImageFile />}
+			{type.includes('video') && <VideoFile />}
+			{type.includes('audio') && <AudioFile />}
+			{type.includes('zip') && <ZipFile />}
+			{type.includes('pdf') && <DocsFile />}
+			{type.includes('officedocument') && <DocsFile />}
 		</IconDiv>
 	);
 };
