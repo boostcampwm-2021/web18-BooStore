47670018951c0579e8c6630041c446fa579425a8
gidskql6671 <gidskql6671@gmail.com>
2021-11-16 19:00:20 +0900

    [Fix] styled-components 폰트 문제 해결
    styled-components에서 등록한 전역 폰트가 적절히 적용되지 않던 버그를 수정함

--- frontend/src/asset/style/global-styles.ts
+++ frontend/src/asset/style/global-styles.ts
@@ -1,6 +1,4 @@
 import { createGlobalStyle } from "styled-components";
-import GmarketSansTTFMedium from '../font/GmarketSansTTFMedium.ttf';
-import GmarketSansTTFBold from '../font/GmarketSansTTFBold.ttf';
 
 const GlobalStyle = createGlobalStyle`
 	html, body {
@@ -10,25 +8,10 @@ const GlobalStyle = createGlobalStyle`
 		margin: 0;
 		padding: 0;
 	}
-    
-    @font-face {
-        font-family: "Gmarket Sans Medium";
-        src: local("Gmarket Sans Medium"), url(${GmarketSansTTFMedium}) format('truetype');
-        font-weight: normal;
-        font-style: normal;
-    }
-    
-    @font-face {
-        font-family: "Gmarket Sans Bold";
-        src: local("Gmarket Sans Bold"), url(${GmarketSansTTFBold}) format('truetype');
-        font-weight: bold;
-        font-style: normal;
-    }
-    
+ 
 	* {
 		box-sizing: border-box;
-        font-family: "Gmarket Sans Medium";
-	}
+	} 
 `;
 
 export default GlobalStyle;
