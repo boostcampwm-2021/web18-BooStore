a8535548fe79a145a3c57b6540b05116fd56aef6
K-moovie <kyha97@naver.com>
2021-11-16 17:27:40 +0900

    [Style] 리소스 추가
    로그인에 필요한 아이콘, 폰트를 추가했습니다.

--- frontend/src/asset/style/theme.ts
+++ frontend/src/asset/style/theme.ts
@@ -10,14 +10,16 @@ const themeValue = {
 		ModalBG: 'rgba(196, 196, 196, 0.5)',
 		Content: '#282828',
 		Point: '#F3AA18',
-		MetaData: '#888888'
+		MetaData: '#888888',
 	},
 	HeaderHeight: '80px',
 	FontFamily: {
+		Light: 'Gmarket Sans Light',
 		Medium: 'Gmarket Sans Medium',
-		Bold: 'Gmarket Sans Bold'
+		Bold: 'Gmarket Sans Bold',
 	},
 	fontSize: {
+		Hint: '14px',
 		Content: '16px',
 		ContentSmall: '14px',
 		Title: '18px',
@@ -26,11 +28,11 @@ const themeValue = {
 		Header: '20px 40px',
 		Content: '20px',
 		Modal: '20px',
-	}
-}
+	},
+};
 
 const defaultTheme: DefaultTheme = {
-	...themeValue
+	...themeValue,
 };
 
 export { defaultTheme, themeValue };
