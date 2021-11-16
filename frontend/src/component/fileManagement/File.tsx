6bc72165d9090764e0d0805048d39d31c759dec7
K-moovie <kyha97@naver.com>
2021-11-16 21:53:51 +0900

    [Style] 파일 리스트 뷰 수정
    새로운 디자인에 맞게 파일 리스트 뷰를 수정했습니다.

--- frontend/src/component/fileManagement/File.tsx
+++ frontend/src/component/fileManagement/File.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as ToggleOffSvg } from '../../asset/image/check_box_outl
 import { ReactComponent as ToggleOnSvg } from '../../asset/image/check_box_outline_selected.svg';
 
 import { FileDTO } from '../../DTO';
-import TypeIcon from './TypeIcon';
+import FileIcon from './FileIcon';
 import { getFiles } from '../../util';
 
 interface Props {
@@ -13,10 +13,16 @@ interface Props {
 	setSelectedFiles: React.Dispatch<React.SetStateAction<FileDTO[]>>;
 	setFiles: React.Dispatch<React.SetStateAction<FileDTO[]>>;
 	setCurrentDir: React.Dispatch<React.SetStateAction<string>>;
-	currentDirectory : string;
+	currentDirectory: string;
 }
 
-const File: React.FC<Props> = ({ file, setSelectedFiles, setFiles, setCurrentDir, currentDirectory }) => {
+const File: React.FC<Props> = ({
+	file,
+	setSelectedFiles,
+	setFiles,
+	setCurrentDir,
+	currentDirectory,
+}) => {
 	const [isSelected, setSelected] = useState(false);
 
 	const { contentType, name, createdAt, updatedAt, size, _id } = file;
@@ -38,50 +44,61 @@ const File: React.FC<Props> = ({ file, setSelectedFiles, setFiles, setCurrentDir
 		});
 	};
 
-	const getChildrenFiles = async( isFolder: boolean, childDirectory: string) =>{
-		if(isFolder){
+	const getChildrenFiles = async (isFolder: boolean, childDirectory: string) => {
+		if (isFolder) {
 			const files = await getFiles(childDirectory);
 			setFiles(files);
 			setCurrentDir(childDirectory);
 			setSelectedFiles([]);
 			setSelected(false);
 		}
-	}
+	};
 
 	useEffect(() => {}, [isSelected]);
 
-	const childDir = currentDirectory==='/'? currentDirectory+name: currentDirectory+'/'+name;
-	const isFolder = contentType==='folder'
+	const childDir =
+		currentDirectory === '/' ? currentDirectory + name : currentDirectory + '/' + name;
+	const isFolder = contentType === 'folder';
+
+	useEffect(() => {}, []);
 
 	return (
 		<Container onClick={onClickFile} isSelected={isSelected}>
-			{isSelected ? <ToggleOnSvg /> : <ToggleOffSvg />}
-			<TypeIcon type={contentType} />
-			<FileName isFolder={isFolder} onClick={()=>getChildrenFiles( isFolder, childDir )}>{name}</FileName>
-			<p> {createdAt} </p>
-			<p> {updatedAt} </p>
-			<p> {size} </p>
+			<p>{isFolder}</p>
+			<FileIcon type={contentType} />
+			<FileName isFolder={isFolder} onClick={() => getChildrenFiles(isFolder, childDir)}>
+				{name}
+			</FileName>
+			<MetaData> {createdAt} </MetaData>
+			<MetaData> {updatedAt} </MetaData>
+			<MetaData> {size} </MetaData>
 		</Container>
 	);
 };
 
 const Container = styled.div<{ isSelected: boolean }>`
 	display: grid;
-	grid-template-columns: 1fr 2fr 10fr 2fr 2fr 2fr;
+	align-items: center;
+	grid-template-columns: 20px 60px 10fr 2fr 2fr 2fr;
 	border-bottom: 1px solid ${(props) => props.theme.color.Line};
 	background-color: ${({ isSelected, theme }) => isSelected && theme.color.SecondaryBG};
-	&:hover{
-		background-color: ${({theme}) => theme.color.SecondaryBG};
+	&:hover {
+		background-color: ${({ theme }) => theme.color.SecondaryBG};
 	}
 `;
 
-const FileName = styled.div<{isFolder: boolean}>`
-	&:hover{
-		${({isFolder}) => `
-			text-decoration: underline;
-			color: #00008B`
-		}	
-	};
-`
+const FileName = styled.div<{ isFolder: boolean }>`
+	font: ${(props) => props.theme.fontSize.Content} ${(props) => props.theme.FontFamily.Medium};
+	color: ${(props) => props.theme.color.Content};
+	&:hover {
+		cursor: default;
+	}
+	margin: auto 0px;
+`;
+
+const MetaData = styled.p`
+	font: ${(props) => props.theme.fontSize.Content} ${(props) => props.theme.FontFamily.Medium};
+	color: ${(props) => props.theme.color.MetaData};
+`;
 
 export default React.memo(File);
--- frontend/src/component/fileManagement/FileList.tsx
+++ frontend/src/component/fileManagement/FileList.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 
 import { ReactComponent as ToggleOffSvg } from '../../asset/image/check_box_outline_blank.svg';
 import { ReactComponent as ToggleOnSvg } from '../../asset/image/check_box_outline_selected.svg';
-import TypeIcon from './TypeIcon';
 import { FileDTO } from '../../DTO';
 import File from './File';
 
@@ -12,23 +11,36 @@ interface Props {
 	setSelectedFiles: React.Dispatch<React.SetStateAction<FileDTO[]>>;
 	setFiles: React.Dispatch<React.SetStateAction<FileDTO[]>>;
 	setCurrentDir: React.Dispatch<React.SetStateAction<string>>;
-	currentDirectory : string;
+	currentDirectory: string;
 }
 
-const FileList: React.FC<Props> = ({ files, setSelectedFiles, setFiles, setCurrentDir, currentDirectory }) => {
+const FileList: React.FC<Props> = ({
+	files,
+	setSelectedFiles,
+	setFiles,
+	setCurrentDir,
+	currentDirectory,
+}) => {
 	return (
 		<Container>
 			<FileHeader>
 				<p></p>
-				<p> 종류 </p>
-				<p> 이름 </p>
-				<p> 올린 날짜 </p>
-				<p> 수정한 날짜 </p>
-				<p> 파일 크기 </p>
+				<p></p>
+				<FileHeaderElement> 이름 </FileHeaderElement>
+				<FileHeaderElement> 올린 날짜 </FileHeaderElement>
+				<FileHeaderElement> 수정한 날짜 </FileHeaderElement>
+				<FileHeaderElement> 파일 크기 </FileHeaderElement>
 			</FileHeader>
 			<Files>
 				{files.map((file, index) => (
-					<File key={index} file={file} setSelectedFiles={setSelectedFiles} setFiles={setFiles} setCurrentDir={setCurrentDir} currentDirectory={currentDirectory}/>
+					<File
+						key={index}
+						file={file}
+						setSelectedFiles={setSelectedFiles}
+						setFiles={setFiles}
+						setCurrentDir={setCurrentDir}
+						currentDirectory={currentDirectory}
+					/>
 				))}
 			</Files>
 		</Container>
@@ -43,12 +55,17 @@ const Container = styled.div`
 
 const gridTemplate = `
 	display: grid;
-	grid-template-columns: 1fr 2fr 10fr 2fr 2fr 2fr;
+	grid-template-columns: 20px 60px 10fr 2fr 2fr 2fr;
 `;
 
 const FileHeader = styled.div`
 	${gridTemplate};
 	border-bottom: 1px solid ${(props) => props.theme.color.Line};
+`;
+
+const FileHeaderElement = styled.p`
+	font: ${(props) => props.theme.fontSize.Content} ${(props) => props.theme.FontFamily.Bold};
+	color: ${(props) => props.theme.color.Content};
 `;
 
 const Files = styled.ul`
