957fd7d233e2bf759a145d160084be2258fcb184
ChipmunkForLove <chipmunk4lov@gmail.com>
2021-11-23 23:52:43 +0900

    [Feat] 중복된 파일명

---
+++ frontend/src/api/newFolder.ts
@@ -0,0 +1,17 @@
+export const handleNewFolder = async (newFolderName: string ,curDir: string)=>{
+    return fetch(`/cloud/newfolder`,{
+        method: 'POST',
+        credentials: 'include',
+        headers : {"Content-Type" : "application/json"},
+        body: JSON.stringify(
+            {
+                "name":{newFolderName},
+                "curdir":{curDir}
+            }
+        )
+    })
+        .then((res) => {
+            return res.json();
+            
+        });
+}
