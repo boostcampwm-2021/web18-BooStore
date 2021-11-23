0389bcce46830ddbfa91c4d4108c799fca821973
gidskql6671 <gidskql6671@gmail.com>
2021-11-24 00:57:41 +0900

    [Fix] 이벤트 리스너 등록 수정

--- frontend/src/component/hook/useContextMenu.ts
+++ frontend/src/component/hook/useContextMenu.ts
@@ -1,29 +1,36 @@
-import { useEffect, useCallback, useState } from "react";
+import { useEffect, useCallback, useState } from 'react';
 
 const useContextMenu = () => {
-  const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
-  const [show, setShow] = useState(false);
+	const [anchorPoint, setAnchorPoint] = useState({ x: 0, y: 0 });
+	const [show, setShow] = useState(false);
 
-  const handleContextMenu = useCallback(
-    (event) => {
-      event.preventDefault();
-      setAnchorPoint({ x: event.pageX, y: event.pageY });
-      setShow(true);
-    },
-    [setShow, setAnchorPoint]
-  );
+	const handleContextMenu = useCallback(
+		(event) => {
+			event.preventDefault();
+			setAnchorPoint({ x: event.pageX, y: event.pageY });
+			setShow(true);
+		},
+		[setShow, setAnchorPoint]
+	);
 
-  const handleClick = useCallback(() => (show ? setShow(false) : null), [show]);
+	const handleClick = useCallback(() => (show ? setShow(false) : null), [show]);
 
-  useEffect(() => {
-    document.addEventListener("click", handleClick);
-    document.addEventListener("contextmenu", handleContextMenu);
-    return () => {
-      document.removeEventListener("click", handleClick);
-      document.removeEventListener("contextmenu", handleContextMenu);
-    };
-  }, []);
-  return { anchorPoint, show };
+	useEffect(() => {
+		document.addEventListener('click', handleClick);
+		console.log('tt');
+		return () => {
+			document.removeEventListener('click', handleClick);
+		};
+	}, [show]);
+
+	useEffect(() => {
+		document.addEventListener('contextmenu', handleContextMenu);
+		return () => {
+			document.removeEventListener('contextmenu', handleContextMenu);
+		};
+	}, [setShow, setAnchorPoint]);
+
+	return { anchorPoint, show };
 };
 
 export default useContextMenu;
