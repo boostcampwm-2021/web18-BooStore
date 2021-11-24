30d81dc08717a121e8dc8888006171683570b642
ChipmunkForLove <chipmunk4lov@gmail.com>
2021-11-24 21:07:05 +0900

    [Chore] update 테스트 중

--- frontend/src/page/MainPage.tsx
+++ frontend/src/page/MainPage.tsx
@@ -58,7 +58,7 @@ const MainPage: React.FC<MainPageProps> = ({ user, setUser }) => {
 	const [selectedFiles, setSelectedFiles] = useState<Map<string, FileDTO>>(new Map());
 	const [isAscending, setIsAscending] = useState<boolean>(true);
 	const location = useLocation<{ currentDirectory: string | undefined }>();
-
+	
 	const onClickDirectory = async (relativePath: string) => {
 		const files = await getFiles(relativePath, isAscending);
 		setFiles(files);
