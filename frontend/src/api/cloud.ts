ca22913bf8bc94933915511b7b7ff76522a35bac
ChipmunkForLove <chipmunk4lov@gmail.com>
2021-11-30 01:10:44 +0900

    [Refactor] contextmenu의 addStar기능과  moveFile에 들어있는 '/cloud/files' api 요청 분리

--- frontend/src/api/cloud.ts
+++ frontend/src/api/cloud.ts
@@ -1,78 +1,10 @@
-import { FileDTO, FileEditAction } from '@DTO';
-
-export const moveFileToTrash = async (selectedFiles: Map<string, FileDTO>) => {
-	const targetIds = [...selectedFiles.values()]
-		.filter((file) => file.contentType !== 'folder')
-		.map((file) => file._id);
-	const directories = [...selectedFiles.values()]
-		.filter((file) => file.contentType === 'folder')
-		.map((file) => ({
-			directory: file.directory,
-			name: file.name,
-		}));
-
-	const body = {
-		targetIds: targetIds,
-		directories: directories,
-		action: FileEditAction.trash,
-	};
-	await fetch('/cloud/files', {
-		method: 'PATCH',
+export const  handleFiles = async ( method: string, body: object)=>{
+	return await fetch('/cloud/files', {
+		method: method,
 		credentials: 'include',
 		headers: {
 			'Content-Type': 'application/json',
 		},
-		body: JSON.stringify(body),
+		body: JSON.stringify(body)
 	});
-};
-
-export const restoreTrashFile = async (selectedFiles: Map<string, FileDTO>) => {
-	const targetIds = [...selectedFiles.values()]
-		.filter((file) => file.contentType !== 'folder')
-		.map((file) => file._id);
-	const directories = [...selectedFiles.values()]
-		.filter((file) => file.contentType === 'folder')
-		.map((file) => ({
-			directory: file.directory,
-			name: file.name,
-		}));
-
-	const body = {
-		targetIds: targetIds,
-		directories: directories,
-		action: FileEditAction.restore,
-	};
-	await fetch('/cloud/files', {
-		method: 'PATCH',
-		credentials: 'include',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(body),
-	});
-};
-
-export const removeFile = async (selectedFiles: Map<string, FileDTO>) => {
-	const targetIds = [...selectedFiles.values()]
-		.filter((file) => file.contentType !== 'folder')
-		.map((file) => file._id);
-	const directories = [...selectedFiles.values()]
-		.filter((file) => file.contentType === 'folder')
-		.map((file) => ({
-			directory: file.directory,
-			name: file.name,
-		}));
-
-	const body = {
-		targetIds: targetIds,
-		directories: directories,
-	};
-	await fetch('/cloud/files', {
-		method: 'DELETE',
-		credentials: 'include',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(body),
-	});
-};
+}
---
+++ frontend/src/api/handleTrashFiles.ts
@@ -0,0 +1,97 @@
+import { FileDTO,  FileEditAction } from '../DTO';
+
+
+export const getTrashFiles = () => {
+	return fetch(`/cloud/trash`, {
+		credentials: 'include',
+		
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			const files: FileDTO[] = data;
+			return files;
+		})
+		.catch((err) => {
+			console.error(err);
+			return [];
+		});
+};
+
+export const moveFileToTrash = async (selectedFiles: Map<string, FileDTO>) => {
+	const targetIds = [...selectedFiles.values()]
+		.filter((file) => file.contentType !== 'folder')
+		.map((file) => file._id);
+	const directories = [...selectedFiles.values()]
+		.filter((file) => file.contentType === 'folder')
+		.map((file) => ({
+			directory: file.directory,
+			name: file.name,
+		}));
+
+	const body = {
+		targetIds: targetIds,
+		directories: directories,
+		action: FileEditAction.trash,
+	};
+	await fetch('/cloud/files', {
+		method: 'PATCH',
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(body),
+	});
+};
+
+export const restoreTrashFile = async (selectedFiles: Map<string, FileDTO>) => {
+	const targetIds = [...selectedFiles.values()]
+		.filter((file) => file.contentType !== 'folder')
+		.map((file) => file._id);
+	const directories = [...selectedFiles.values()]
+		.filter((file) => file.contentType === 'folder')
+		.map((file) => ({
+			directory: file.directory,
+			name: file.name,
+		}));
+
+	const body = {
+		targetIds: targetIds,
+		directories: directories,
+		action: FileEditAction.restore,
+	};
+	await fetch('/cloud/files', {
+		method: 'PATCH',
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(body),
+	});
+};
+
+export const removeFile = async (selectedFiles: Map<string, FileDTO>) => {
+	const targetIds = [...selectedFiles.values()]
+		.filter((file) => file.contentType !== 'folder')
+		.map((file) => file._id);
+	const directories = [...selectedFiles.values()]
+		.filter((file) => file.contentType === 'folder')
+		.map((file) => ({
+			directory: file.directory,
+			name: file.name,
+		}));
+
+	const body = {
+		targetIds: targetIds,
+		directories: directories,
+	};
+	await fetch('/cloud/files', {
+		method: 'DELETE',
+		credentials: 'include',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(body),
+	});
+};
