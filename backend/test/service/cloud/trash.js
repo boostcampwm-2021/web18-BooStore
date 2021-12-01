165761406853a1605b40119c86ebac1e5030ae80
gidskql6671 <gidskql6671@gmail.com>
2021-12-01 15:53:59 +0900

    [Test] 파일을 휴지통으로 이동하거나 빼는 함수의 Unit Test 작성
    - moveFilesToTrash
    - restoreTrashFiles

--- backend/test/service/cloud/trash.js
+++ backend/test/service/cloud/trash.js
@@ -2,12 +2,47 @@ const assert = require('assert');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { createUser } = require('../../../src/service');
-const { getTrashFiles } = require('../../../src/service/cloud');
+const {
+	getTrashFiles,
+	moveFilesToTrash,
+	restoreTrashFiles,
+	moveFoldersToTrash,
+	restoreTrashFolders,
+} = require('../../../src/service/cloud');
 const { Cloud, User } = require('../../../src/model');
 
-describe('service/cloud/trash.ts', function () {
-	const loginId = 'testCodeUser@';
+const loginId = 'testCodeUser@';
+const initCloudDocs = async () => {
+	/*
+	- /folder1
+	- /folder1/folder2     <- 삭제됨
+	- /folder1/file1.txt   <- 삭제됨
+	*/
+	await Cloud.create(
+		{
+			ownerId: loginId,
+			directory: '/',
+			name: 'folder1',
+			contentType: 'folder',
+		},
+		{
+			ownerId: loginId,
+			directory: '/folder1',
+			name: 'folder2',
+			contentType: 'folder',
+			isDeleted: true,
+		},
+		{
+			ownerId: loginId,
+			directory: '/folder1',
+			name: 'file1.txt',
+			contentType: 'text/plain',
+			isDeleted: true,
+		}
+	);
+};
 
+describe('service/cloud/trash.ts', function () {
 	before('connect', function (done) {
 		mongoose.connect(
 			'mongodb://localhost:27017/BooStore',
@@ -29,35 +64,7 @@ describe('service/cloud/trash.ts', function () {
 	});
 
 	describe('getTrashFiles', function () {
-		beforeEach('make documents', async () => {
-			/*
-			- /folder1
-			- /folder1/folder2     <- 삭제됨
-			- /folder1/file1.txt   <- 삭제됨
-			*/
-			await Cloud.create(
-				{
-					ownerId: loginId,
-					directory: '/',
-					name: 'folder1',
-					contentType: 'folder',
-				},
-				{
-					ownerId: loginId,
-					directory: '/folder1',
-					name: 'folder2',
-					contentType: 'folder',
-					isDeleted: true,
-				},
-				{
-					ownerId: loginId,
-					directory: '/folder1',
-					name: 'file1.txt',
-					contentType: 'text/plain',
-					isDeleted: true,
-				}
-			);
-		});
+		beforeEach('make documents', initCloudDocs);
 		afterEach('delete documents', async () => {
 			await Cloud.deleteMany({});
 		});
@@ -98,6 +105,72 @@ describe('service/cloud/trash.ts', function () {
 
 			// then
 			assert.equal(directories.length, expected);
+		});
+	});
+	describe('moveFilesToTrash', function () {
+		beforeEach('make documents', initCloudDocs);
+		afterEach('delete documents', async () => {
+			await Cloud.deleteMany({});
+		});
+
+		it('파일을 쓰레기통으로 이동', async () => {
+			// given
+			const file = await Cloud.findOneAndUpdate(
+				{
+					directory: '/folder1',
+					name: 'file1.txt',
+				},
+				{
+					isDeleted: false,
+				},
+				{
+					new: true,
+				}
+			);
+			const input = {
+				userLoginId: loginId,
+				targetIds: [file._id],
+			};
+			const expected = true;
+
+			// when
+			await moveFilesToTrash(input);
+			const updatedFile = await Cloud.findOne({
+				directory: '/folder1',
+				name: 'file1.txt',
+			});
+
+			// then
+			assert.equal(updatedFile.isDeleted, expected);
+		});
+	});
+	describe('restoreTrashFiles', function () {
+		beforeEach('make documents', initCloudDocs);
+		afterEach('delete documents', async () => {
+			await Cloud.deleteMany({});
+		});
+
+		it('쓰레기통에 있는 파일을 복구', async () => {
+			// given
+			const file = await Cloud.findOne({
+				directory: '/folder1',
+				name: 'file1.txt',
+			});
+			const input = {
+				userLoginId: loginId,
+				targetIds: [file._id],
+			};
+			const expected = false;
+
+			// when
+			await restoreTrashFiles(input);
+			const updatedFile = await Cloud.findOne({
+				directory: '/folder1',
+				name: 'file1.txt',
+			});
+
+			// then
+			assert.equal(updatedFile.isDeleted, expected);
 		});
 	});
 });
